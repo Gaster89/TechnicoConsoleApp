45073385f93fbb81af0e556b4ea2d996019f0cb9
Dimitris Gasteratos <dgasteratos@plaisio.gr>
2024-11-26 13:19:02 +0200

    Create PropertyOwnerService

---
+++ TechnicoConsoleApp/Interfaces/IPropertyOwnerService.cs
@@ -0,0 +1,11 @@
+﻿using TechnicoConsoleApp.Models;
+
+namespace TechnicoConsoleApp.Interfaces;
+
+public interface IPropertyOwnerService
+{
+    Task<PropertyOwner> RegisterAsync(PropertyOwner propertyOwner);
+    Task<PropertyOwner> GetOwnerDetailsAsync(int id);
+    Task UpdateOwnerAsync(PropertyOwner propertyOwner);
+    Task DeleteOwnerAsync(int id);
+}
---
+++ TechnicoConsoleApp/Services/PropertyOwnerService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechnicoConsoleApp.Context;
+using TechnicoConsoleApp.Interfaces;
+using TechnicoConsoleApp.Models;
+
+namespace TechnicoConsoleApp.Services;
+
+public class PropertyOwnerService : IPropertyOwnerService
+{
+    private readonly ApplicationDbContext _context;
+
+    public PropertyOwnerService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PropertyOwner> RegisterAsync(PropertyOwner propertyOwner)
+    {
+        _context.PropertyOwners.Add(propertyOwner);
+        await _context.SaveChangesAsync();
+        return propertyOwner;
+    }
+
+    public async Task<PropertyOwner> GetOwnerDetailsAsync(int id)
+    {
+        return await _context.PropertyOwners.Include(p => p.Properties).FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task UpdateOwnerAsync(PropertyOwner propertyOwner)
+    {
+        _context.PropertyOwners.Update(propertyOwner);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteOwnerAsync(int id)
+    {
+        PropertyOwner? owner = await _context.PropertyOwners.FindAsync(id);
+        _context.PropertyOwners.Remove(owner);
+        await _context.SaveChangesAsync();
+    }
+}
