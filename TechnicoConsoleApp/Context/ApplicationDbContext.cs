3958ea5eac9c3525e75e3c81880ff6aae9f00a00
Dimitris Gasteratos <dgasteratos@plaisio.gr>
2024-11-26 12:57:30 +0200

    Create Database Context

---
+++ TechnicoConsoleApp/Context/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechnicoConsoleApp.Models;
+
+namespace TechnicoConsoleApp.Context;
+
+// ApplicationDbContext.cs
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<PropertyOwner> PropertyOwners { get; set; }
+    public DbSet<PropertyItem> PropertyItems { get; set; }
+    public DbSet<Repair> Repairs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<PropertyOwner>()
+            .HasMany(p => p.Properties)
+            .WithOne(p => p.PropertyOwner)
+            .HasForeignKey(p => p.PropertyOwnerId);
+
+        modelBuilder.Entity<PropertyItem>()
+            .HasMany(p => p.Repairs)
+            .WithOne(r => r.PropertyItem)
+            .HasForeignKey(r => r.PropertyItemId);
+    }
+}
